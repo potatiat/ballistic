98b06ec001b7396395d6cf7788b503fdec50f541
Ronald Caesar <github43132@proton.me>
2026-06-03 01:59:12 -0400

    x86: add NCZV flags
    They are stored as contiguous 8-bit values to allow fast setcc writes
    without read-modify-write stalls.
    
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- include/backend/bal_cpu.h
+++ include/backend/bal_cpu.h
@@ -16,6 +16,14 @@ extern "C"
         uint64_t x[32];
         uint64_t pc;
 
+        // ARM's Carry flag is inverted compared to x86's Carry during subtraction operations.
+        // Compiler's must handle this.
+        uint8_t  flag_c; // ARM C -> x86 CF
+        uint8_t  flag_z; // ARM Z -> x86 ZF
+        uint8_t  flag_n; // ARM N -> x86 SF
+        uint8_t  flag_v; // ARM V -> x86 OF
+        uint32_t _pad_flags;
+
         /// Tracks executed guest instructions.
         ///
         /// This only increments if the engine flag [`BAL_ENGINE_FLAG_INSTRUCTION_COUNTING`] is set.
