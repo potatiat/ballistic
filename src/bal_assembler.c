036c3d3cb4f42d04e6eee159b21d2325aaa9cbd0
Ronald Caesar <github43132@proton.me>
2026-06-06 07:10:57 -0400

    assembler: update log statement in SUB (Imm)
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- src/bal_assembler.c
+++ src/bal_assembler.c
@@ -346,7 +346,7 @@ bal_emit_sub_immediate(bal_assembler_t           *assembler,
 
     if (rd > 31)
     {
-        BAL_LOG_ERROR(&assembler->logger, "X%u out of range (0-31).", rd);
+        BAL_LOG_ERROR(&assembler->logger, "Rd X%u out of range (0-31).", rd);
         assembler->status = BAL_ERROR_INVALID_ARGUMENT;
         return;
     }
