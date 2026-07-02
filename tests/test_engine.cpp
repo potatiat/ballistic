59a1b0bae0415d0e3ed774bbb0318b6938089b70
Ronald Caesar <github43132@proton.me>
2026-07-02 00:26:35 -0400

    [35] tests: add very first integration test
    This translates the following code to x86:
    
    // X1 = Accumulator
    MOVZ X0, #0x9896
    MOVK X0, #0x80, LSL #16  // X0 = 10,000,000
    MOVZ X1, #0
    
    .loop:
    SUBS X0, X0, #1          // Decrement counter and set flags
    ADD  X1, X1, X0          // Accumulate
    B.NE .loop
    
    RET
    
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -4,6 +4,54 @@ extern "C"
 #include "../src/bal_engine.c"
 }
 
+TEST(EngineIntegration, WhileLoop)
+{
+    bal_cpu_t       cpu       = {};
+    bal_allocator_t allocator = {};
+    bal_allocator_default_init(&allocator);
+
+    bal_logger_t logger = {};
+    bal_logger_init_default(&logger);
+
+    const size_t guest_memory_size = 4096;
+    uint32_t    *guest_memory
+        = (uint32_t *)allocator.allocate(allocator.context, 16, guest_memory_size);
+    ASSERT_NE(guest_memory, nullptr);
+    memset(guest_memory, 0, guest_memory_size);
+
+    bal_memory_interface_t memory_interface = {};
+    bal_error_t            error            = bal_flat_translation_interface_init(
+        &allocator, &memory_interface, guest_memory, guest_memory_size, logger);
+    ASSERT_EQ(error, BAL_SUCCESS);
+
+    bal_engine_t engine = {};
+    error               = bal_engine_init(&engine, &cpu, &allocator, &memory_interface, logger);
+    ASSERT_EQ(error, BAL_SUCCESS);
+
+    guest_memory[0] = 0xD292D000; // MOVZ X0, #0x9680
+    guest_memory[1] = 0xF2A01300; // MOVK X0, #0x98, LSL #16 (X0 = 10,000,000)
+    guest_memory[2] = 0xD2800010; // MOVZ X1, #0
+    // .loop:
+    guest_memory[3] = 0xF1000400; // SUBS X0, X0, #1
+    guest_memory[4] = 0x8B000021; // ADD X1, X1, X0
+    guest_memory[5] = 0x54FFFFC1; // B.NE .loop
+    guest_memory[6] = 0xD65F03C0; // RET (X30)
+
+    cpu.pc    = 0;
+    cpu.x[30] = BAL_ENGINE_SENTINEL;
+    error     = bal_engine_run_thread(&engine);
+    EXPECT_EQ(error, BAL_SUCCESS);
+
+    // Sum = N * (N - 1) / 2 where N = 10,000,000
+    const uint64_t expected_sum = 49999995000000ULL;
+    EXPECT_EQ(cpu.x[1], expected_sum);
+    EXPECT_EQ(cpu.x[0], 0);
+    // .loop:
+
+    bal_flat_translation_interface_destroy(&allocator, &memory_interface);
+    allocator.free(allocator.context, guest_memory, guest_memory_size);
+}
+
 class EngineBlockCache : public testing::Test
 {
 protected:
