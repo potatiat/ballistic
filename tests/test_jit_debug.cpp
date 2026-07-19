1491eadda20e08d69263d432d1fcc6acb73b8612
Ronald Caesar <github43132@proton.me>
2026-07-19 03:01:13 -0400

    [63] tests/debug: fix clion warnings in add_block_success test

--- tests/test_jit_debug.cpp
+++ tests/test_jit_debug.cpp
@@ -42,8 +42,8 @@ TEST_F(JitDebug, Destroy_Success)
 TEST_F(JitDebug, AddBlock_Success)
 {
     bal_jit_debug_init(&allocator, &context, logger);
-    bal_jit_instruction_map_t map      = { 0, 0 };
-    void                     *dummy_rx = (void *)0x1000;
+    const bal_jit_instruction_map_t map      = { 0, 0 };
+    const auto                      dummy_rx = reinterpret_cast<void *>(0x1000);
 
     EXPECT_EQ(bal_jit_debug_add_block(&context, dummy_rx, 64, 0x2000, &map, 1), BAL_SUCCESS);
     EXPECT_EQ(context.entry_count, 1);
