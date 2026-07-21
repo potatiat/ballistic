ddcc588a1865f730e9a8f6135b8930b1c2eeafa0
Ronald Caesar <github43132@proton.me>
2026-07-21 01:58:43 -0400

    [71] ci: add the mold linker as a dependancy for debug builds

--- tools/scripts/setup_dependencies.sh
+++ tools/scripts/setup_dependencies.sh
@@ -6,10 +6,10 @@ PACKAGE_MANAGER="${1:?Usage: setup_deps.sh <apt|brew>}"
 case "$PACKAGE_MANAGER" in
     apt)
         sudo apt-get update -qq
-        sudo apt-get install -y --no-install-recommends libclang-18-dev ccache ninja-build libwayland-dev libxkbcommon-dev xorg-dev
+        sudo apt-get install -y --no-install-recommends libclang-18-dev ccache ninja-build libwayland-dev libxkbcommon-dev xorg-dev mold
         ;;
     brew)
-        brew install ccache ninja
+        brew install ccache ninja mold
         ;;
     *)
         echo "Unsupported package manager: $PACKAGE_MANAGER" >&2
