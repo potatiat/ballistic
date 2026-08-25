07c7f2b566057ea548715fa1fea83d888f40fda2
Ronald Caesar <github43132@proton.me>
2026-08-25 16:09:42 -0400

    [78] tests/assembler: implement setup function

--- tests/unit/bal_assembler/test_bal_assembler_lifecycle.c
+++ tests/unit/bal_assembler/test_bal_assembler_lifecycle.c
@@ -1,9 +1,20 @@
 #include "bal_assembler.h"
 #include "unity.h"
+#include <string.h>
+
+#define TEST_BUFFER_CAPACITY 128
+
+static bal_assembler_t assembler;
+static uint32_t        code_buffer[TEST_BUFFER_CAPACITY];
+static bal_logger_t    silent_logger;
 
 void
 setUp(void)
 {
+    memset(&assembler, 0, sizeof(assembler));
+    memset(&code_buffer, 0, sizeof(code_buffer));
+    memset(&silent_logger, 0, sizeof(silent_logger));
+    silent_logger.min_level = BAL_LOG_LEVEL_NONE;
 }
 
 void
