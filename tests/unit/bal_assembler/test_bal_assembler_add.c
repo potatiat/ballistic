51214d06fb452ed3205ae0b1afb88ba89b8afce5
Ronald Caesar <github43132@proton.me>
2026-08-28 02:49:47 -0400

    [78] tests/assembler: remove add immediate success test
    A success test already exist inside bal_assembler.h. It gets ran
    with `doctest.lua`.

--- tests/unit/bal_assembler/test_bal_assembler_add.c
+++ tests/unit/bal_assembler/test_bal_assembler_add.c
@@ -86,16 +86,6 @@ test_BalAssemblerEmitAddImmediate_FullBufferReturnsErrorInstructionOverflow(void
 }
 
 static void
-test_BalAssemblerEmitAddImmediate_Success(void)
-{
-    (void)bal_test_assembler_init_valid();
-    bal_emit_add_immediate(&bal_test_assembler, BAL_REGISTER_X0, BAL_REGISTER_X1, 42U, 1U);
-    TEST_ASSERT_EQUAL(BAL_SUCCESS, bal_test_assembler.status);
-    TEST_ASSERT_EQUAL_size_t(1, bal_test_assembler.offset);
-    TEST_ASSERT_EQUAL_HEX32(0x9140A820, bal_test_code_buffer[0]);
-}
-
-static void
 test_BalAssemblerEmitAddShiftRegister_NullAssemblerDoesNotCrash(void)
 {
     bal_emit_add_shifted_register(NULL, BAL_REGISTER_X0, BAL_REGISTER_X0, BAL_REGISTER_X0, 0U, 0U);
@@ -206,7 +196,6 @@ main(void)
     RUN_TEST(test_BalAssemblerEmitAddImmediate_ImmOutOfRangeErrorInvalidArgument);
     RUN_TEST(test_BalAssemblerEmitAddImmediate_ShiftOutOfRangeErrorInvalidArgument);
     RUN_TEST(test_BalAssemblerEmitAddImmediate_FullBufferReturnsErrorInstructionOverflow);
-    RUN_TEST(test_BalAssemblerEmitAddImmediate_Success);
     RUN_TEST(test_BalAssemblerEmitAddShiftRegister_NullAssemblerDoesNotCrash);
     RUN_TEST(test_BalAssemblerEmitAddShiftRegister_NullBufferReturnsErrorInvalidArgument);
     RUN_TEST(test_BalAssemblerEmitAddShiftedRegister_DeadMagicReturnsErrorStructCorrupted);
