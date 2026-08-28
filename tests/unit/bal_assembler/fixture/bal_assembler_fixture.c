c907f70970bfb3227176ac31c09e8c5e3ed505e3
Ronald Caesar <github43132@proton.me>
2026-08-27 20:34:53 -0400

    [78] tests/assembler: make fixture more misra compliant

--- tests/unit/bal_assembler/fixture/bal_assembler_fixture.c
+++ tests/unit/bal_assembler/fixture/bal_assembler_fixture.c
@@ -1,22 +1,27 @@
 #include "bal_assembler_fixture.h"
 #include "bal_assembler.h"
+#include "bal_safety.h"
+
 #include <string.h>
 
 bal_assembler_t bal_test_assembler                         = { 0 };
-uint32_t        bal_test_code_buffer[TEST_BUFFER_CAPACITY] = { 0 };
-void           *bal_test_unaligned_pointer                 = NULL;
+uint32_t        bal_test_code_buffer[TEST_BUFFER_CAPACITY] = {};
+void *          bal_test_unaligned_pointer                 = NULL;
 
 void
 setUp(void)
 {
-    memset(&bal_test_assembler, 0, sizeof(bal_test_assembler));
-    memset(&bal_test_code_buffer, 0, sizeof(bal_test_code_buffer));
+    (void)memset(&bal_test_assembler, 0, sizeof(bal_test_assembler));
+    (void)memset(&bal_test_code_buffer, 0, sizeof(bal_test_code_buffer));
     bal_test_unaligned_pointer  = bal_test_code_buffer;
     bal_thread_logger.min_level = BAL_LOG_LEVEL_NONE;
 
-    if (0 == (uintptr_t)bal_test_unaligned_pointer % 4)
+    if (true == bal_pointer_is_aligned(bal_test_unaligned_pointer, 4U))
     {
-        bal_test_unaligned_pointer += 1;
+        uintptr_t address;
+        (void)memcpy(&address, &bal_test_unaligned_pointer, sizeof(address));
+        address += 1U;
+        (void)memcpy(&bal_test_unaligned_pointer, &address, sizeof(address));
     }
 }
 
