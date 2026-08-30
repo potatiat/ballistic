5371ba75e07428a42c1783de7729797546463408
Ronald Caesar <github43132@proton.me>
2026-08-30 08:32:20 -0400

    [78] tests/fuzzer: add struct to track the amount of divergences
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- tests/fuzzer/include/bal_fuzzer_state.h
+++ tests/fuzzer/include/bal_fuzzer_state.h
@@ -4,21 +4,41 @@
 #include "bal_fuzzer_protocol.h"
 #include <stdbool.h>
 
+#define BAL_FUZZER_MAX_DIVERGENCES 40U
+
+typedef enum
+{
+    BAL_FUZZER_FIELD_REGISTER = 0,
+    BAL_FUZZER_FIELD_PC,
+    BAL_FUZZER_FIELD_FLAG_C,
+    BAL_FUZZER_FIELD_FLAG_Z,
+    BAL_FUZZER_FIELD_FLAG_N,
+    BAL_FUZZER_FIELD_FLAG_V,
+} bal_fuzzer_field_t;
+
 typedef struct
 {
+
     /// Value from Unicorn.
     uint64_t expected_value;
 
     /// Value from Ballistic.
     uint64_t actual_value;
 
-    /// X register index.
-    uint32_t first_mismatch_register;
+    /// X register index, or UINT32_MAX if not a GPR.
+    uint32_t register_index;
 
-    /// True if states are identical.
-    bool     match;
-    char     pad0[3];
+    /// Which field diverged.
+    bal_fuzzer_field_t field;
+
     uint64_t pad1;
+} bal_fuzzer_divergence_t;
+
+typedef struct
+{
+    bool                    match;
+    bal_fuzzer_divergence_t divergences[BAL_FUZZER_MAX_DIVERGENCES];
+    uint32_t                divergence_count;
 } bal_fuzzer_comparison_result_t;
 
 void bal_fuzzer_state_capture(bal_fuzzer_cpu_snapshot_t *BAL_RESTRICT snapshot,
